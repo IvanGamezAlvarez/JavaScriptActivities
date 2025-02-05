c2fc3c22e004277c1284e8a6bca005ac094a3468
Ivan <thecubeg@gmail.com>
2025-02-04 21:11:39 -0600

    Advance

--- Ejercicio2/app.js
+++ Ejercicio2/app.js
@@ -2,23 +2,58 @@ const aditionNumber = document.getElementById("adition-button")
 const thermometerButton = document.getElementById("termometro-button")
 const guessButton = document.getElementById("guess-button")
 const number = document.getElementById("number")
+const attemptsText = document.getElementById("attempts-text")
+const npcNumber = document.getElementById("npc-number")
+
+let LightAttemps = 2
+let initailNumber = 0
+
+
+const gameNumber = Math.floor(Math.random() * 100) + 1;
 
 
 aditionNumber.addEventListener("click",()=>{
-    
     let numberToAdd = prompt("Ingrese un numero para sumar")
-    let initailNumber = 0
     initailNumber += Number(numberToAdd)
-    number.textContent = initailNumber
-
+    if (initailNumber > 100){
+        initailNumber = 100
     }
+    number.textContent = initailNumber
+    
+}
 )
 thermometerButton.addEventListener("click",()=>{
-    
-    let numberToAdd = prompt("Ingrese un numero para sumar")
-    let initailNumber = 0
-    initailNumber += Number(numberToAdd)
-    number.textContent = initailNumber
-    
+    if (LightAttemps > 0){
+        LightAttemps -= 1
+        attemptsText.textContent = `Te quedan ${LightAttemps} usos del termometro`
+      
+        let diferentionPlus =  gameNumber - initailNumber
+        let diferentionLess = initailNumber - gameNumber
+        if (diferentionLess < 21 && diferentionLess > 0|| diferentionPlus < 21 && diferentionPlus > 0){
+            number.style.color = "Green"
+        }
+        else if(diferentionLess < 31 && diferentionLess > 0|| diferentionPlus < 31 && diferentionPlus > 0){
+            number.style.color = "Yellow"
+        }
+        else if(diferentionLess < 41 && diferentionLess > 0|| diferentionPlus < 41 && diferentionPlus > 0){
+            number.style.color = "Red"
+        }
+
+    } else{
+        attemptsText.textContent = "te quedaste sin intentos"   
+        }
+    }
+)
+guessButton.addEventListener("click",()=>{
+    let diferentionPlus =  gameNumber - initailNumber
+    let diferentionLess = initailNumber - gameNumber
+    npcNumber.textContent = gameNumber
+        if (diferentionLess < 11 && diferentionLess > 0|| diferentionPlus < 11 && diferentionPlus > 0){
+            attemptsText.textContent = "Ganaste"   
+        } 
+        else {
+            attemptsText.textContent = "Perdiste"   
+        }
+
     }
 )

c958fecf05678ae9295a38c3c7d327ad61da38ad
Ivan <thecubeg@gmail.com>
2025-02-25 20:26:56 -0600

    dd

--- Ejercicio3/app.js
+++ Ejercicio3/app.js
@@ -1,14 +1,11 @@
 let score = 0
-let rightAnswers = 0
-let scoreToWin = 10
-let alreadyWin = false
+let lastCard
+let oldCardsIndexs = []
 const numberOfCards = 13
 const numberOfSymbols = 4
-let lastCard
+const scoreToWin = 3
 
-
-let oldCardsIndexs = []
-
+//this funcion make a number a symbol
 function MakeSymbol(symbol) {
     if (symbol == "1") {
         return "Hearth"
@@ -18,74 +15,65 @@ function MakeSymbol(symbol) {
         return "Clover"
     } else if (symbol == "4") {
         return "Spade"
-    }  
+    }
 }
 
 function VerifyCard(newCard) {
-    return  oldCardsIndexs.includes(newCard)
+    return oldCardsIndexs.includes(newCard)
 }
-
 
 function RegisterCard(card) {
     oldCardsIndexs.unshift(card)
 }
 
-
 function GetARandomCard() {
-    let newNumber = Math.floor(Math.random() * (numberOfCards  ) +1) ;
-    let newSymbol = Math.floor(Math.random() * (numberOfSymbols  ) +1)  ;
+    let newNumber = Math.floor(Math.random() * (numberOfCards) + 1);
+    let newSymbol = Math.floor(Math.random() * (numberOfSymbols) + 1);
     let newCard = newNumber + " " + MakeSymbol(newSymbol)
-    
     lastNumber = newNumber
     lastCard = newCard
     RegisterCard(newCard)
     return newCard
-
 }
 
-
-function ValidateCard(state, beforeCard){
+function ValidateCard(state, beforeCard) {
     const newCard = GetARandomCard()
     newCardSplitted = newCard.split(" ")
     beforeCardSplitted = beforeCard.split(" ")
-
-    
-    let newState = parseInt(newCardSplitted[1]) > parseInt(beforeCardSplitted[1])
-    alert(`la carta anterior fue ${parseInt(beforeCardSplitted[1])} la carta nueva fue  ${parseInt(newCardSplitted[1])} el estado que mandaste fue ${state} y el estado que due es ${newState}`)
-    if(parseInt(newCardSplitted[1]) == parseInt(beforeCardSplitted[1])){
+    let newState = parseInt(newCardSplitted[0]) > parseInt(beforeCardSplitted[0])
+    alert(`la carta anterior fue ${parseInt(beforeCardSplitted[0])} la carta nueva fue  ${parseInt(newCardSplitted[0])} el estado que mandaste es: ${state} y e es estado correcto es: ${newState}`)
+    if (parseInt(newCardSplitted[0]) == parseInt(beforeCardSplitted[0])) {
         alert(`La carta fue del mismo valor, la carta nueva fue ${newCard}`)
     }
-    else if( state  == newState){
+    else if (state == newState) {
         score += 1
         alert(`Adivinaste tu nuevo puntaje es de ${score}, la carta nueva fue ${newCard}`)
     }
-    else{
+    else {
         alert(`Perdiste, la carta nueva fue ${newCard}`)
     }
 }
 
-
-
+//this funcion have the process for ask for the next card  and send the validation
 function GuessCard(newCard) {
-    const nextCard = prompt(`La siguiente carta sera "Mayor o Menor" escriba su respuesta, la carta anterior fue ${newCard}`)
-    if (nextCard == "Mayor"){
+    const nextCard = prompt(`La siguiente carta sera "Mayor o Menor" escriba su respuesta, la carta actual es: ${newCard}`)
+    if (nextCard == "Mayor") {
         ValidateCard(true, newCard)
-    } else if (nextCard == "Menor"){
+    } else if (nextCard == "Menor") {
         ValidateCard(false, newCard)
-
-    } else{
+    } else {
         alert(`${nextCard}  no es un comando valido asegurate de haber escrito "Mayor" o "Menor"`)
         GuessCard()
     }
 }
 
+//this funcion start the game
 function StartGame() {
-    let newCard = GetARandomCard()
-
-    while (score  < scoreToWin) {
-        GuessCard(lastCard)        
+    GetARandomCard()
+    while (score < scoreToWin) {
+        GuessCard(lastCard)
     }
+    alert("Felicidades ganaste el juego!")
 }
-
 
 StartGame()

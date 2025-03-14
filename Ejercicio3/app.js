43dcdf4381728fcfd66ad2e8e51452ea76994e09
Ivan <thecubeg@gmail.com>
2025-03-13 22:28:29 -0600

    js

--- Ejercicio3/app.js
+++ Ejercicio3/app.js
@@ -1,11 +1,15 @@
+//Variables
 let score = 0
 let lastCard
 let oldCardsIndexs = []
+let gameEnd = false
 const numberOfCards = 13
 const numberOfSymbols = 4
-const scoreToWin = 3
+const scoreToWin = 10
 
-//this funcion make a number a symbol
+//Functions
+
+//this funcion make a number to symbol
 function MakeSymbol(symbol) {
     if (symbol == "1") {
         return "Hearth"
@@ -18,43 +22,46 @@ function MakeSymbol(symbol) {
     }
 }
 
-function VerifyCard(newCard) {
-    return oldCardsIndexs.includes(newCard)
-}
+// function VerifyCard(newCard) {
+//     return oldCardsIndexs.includes(newCard)
+// }
 
-function RegisterCard(card) {
-    oldCardsIndexs.unshift(card)
-}
+// function RegisterCard(card) {
+//     oldCardsIndexs.unshift(card)
+// }
 
 function GetARandomCard() {
-    let newNumber = Math.floor(Math.random() * (numberOfCards) + 1);
-    let newSymbol = Math.floor(Math.random() * (numberOfSymbols) + 1);
-    let newCard = newNumber + " " + MakeSymbol(newSymbol)
-    lastNumber = newNumber
+    const newNumber = Math.floor(Math.random() * (numberOfCards) + 1);
+    const newSymbol = Math.floor(Math.random() * (numberOfSymbols) + 1);
+    const newCard = newNumber + " " + MakeSymbol(newSymbol)
     lastCard = newCard
-    RegisterCard(newCard)
     return newCard
 }
 
+//this function valide the answer and increment the score or finish the game
 function ValidateCard(state, beforeCard) {
     const newCard = GetARandomCard()
     newCardSplitted = newCard.split(" ")
     beforeCardSplitted = beforeCard.split(" ")
     let newState = parseInt(newCardSplitted[0]) > parseInt(beforeCardSplitted[0])
-    alert(`la carta anterior fue ${parseInt(beforeCardSplitted[0])} la carta nueva fue  ${parseInt(newCardSplitted[0])} el estado que mandaste es: ${state} y e es estado correcto es: ${newState}`)
     if (parseInt(newCardSplitted[0]) == parseInt(beforeCardSplitted[0])) {
-        alert(`La carta fue del mismo valor, la carta nueva fue ${newCard}`)
+        alert(`La carta fue del mismo valor, la carta nueva fue ${newCard} no recibiste ningun punto intentalo de nuevo`)
     }
     else if (state == newState) {
         score += 1
-        alert(`Adivinaste tu nuevo puntaje es de ${score}, la carta nueva fue ${newCard}`)
+        alert(`la carta nueva fue ${newCard}, Adivinaste tu nuevo puntaje es de ${score}`)
+        if (score == scoreToWin) {
+            alert("Ganaste!")
+            gameEnd = true
+        }
     }
     else {
-        alert(`Perdiste, la carta nueva fue ${newCard}`)
+        alert(`la carta nueva fue ${newCard}, Perdiste`)
+        gameEnd = true
     }
 }
 
-//this funcion have the process for ask for the next card  and send the validation
+//this funcion have the process for ask for the next card and send the validation
 function GuessCard(newCard) {
     const nextCard = prompt(`La siguiente carta sera "Mayor o Menor" escriba su respuesta, la carta actual es: ${newCard}`)
     if (nextCard == "Mayor") {
@@ -62,18 +69,28 @@ function GuessCard(newCard) {
     } else if (nextCard == "Menor") {
         ValidateCard(false, newCard)
     } else {
-        alert(`${nextCard}  no es un comando valido asegurate de haber escrito "Mayor" o "Menor"`)
-        GuessCard()
+        alert(`${nextCard}  no es un comando valido asegurate de haber escrito "Mayor" o "Menor"  recuerda respetar las mayusculas`)
+        GuessCard(newCard)
     }
 }
 
 //this funcion start the game
 function StartGame() {
     GetARandomCard()
-    while (score < scoreToWin) {
+    while (gameEnd != true) {
         GuessCard(lastCard)
     }
-    alert("Felicidades ganaste el juego!")
+    alert(`el juego termino tu puntaje fue ${score}`)
+    const playAgain = confirm("¿Quieres volver a jugar?")
+    if (playAgain) {
+        score = 0
+        gameEnd = false
+        StartGame()
+    } else {
+        alert(`Gracias por jugar`)
+    }
 }
+
+//Calls
 
 StartGame()
